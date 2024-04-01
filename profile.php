52a3e3bb522780961a02ee52a4e2c027141bec63
DavidWarner85 <david.edward.warner@gmail.com>
2024-04-01 22:50:01 +0000

    img file added

--- profile.php
+++ profile.php
@@ -12,6 +12,7 @@
         <div id="top_bar">
             <div style="width: 800px;margin: auto; font-size: 30px;">
             fakebuk &nbsp;<input type="text" id="search_box" placeholder="Search">
+            <img src="img/Naomi.jpg" alt="Selfi of Naomi" style="height: 50px; float: right;">
             </div>
         </div>
         <script src="" async defer></script>
