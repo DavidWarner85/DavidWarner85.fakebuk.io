914984503156794c3b53ddda6d5fe751dc958457
DavidWarner85 <david.edward.warner@gmail.com>
2024-04-03 23:05:49 +0000

    Friends Bar

--- profile.php
+++ profile.php
@@ -9,12 +9,65 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <!-- Top Bar -->
         <div id="top_bar">
             <div style="width: 800px;margin: auto; font-size: 30px;">
             fakebuk &nbsp;<input type="text" id="search_box" placeholder="Search">
             <img src="img/Naomi.jpg" alt="Selfi of Naomi" style="height: 50px; float: right;">
             </div>
         </div>
+       
+        <!--Top Content-->
+        <div id="content_area">
+            <div id="cover_image">
+                <img src="img/city.jpg" style="width: 100%;">
+                <img id="profile_pic" src="img/Naomi.jpg">
+                <br>
+            <div>
+                Naomi Hightower
+            </div>
+                <div id="menu_button">Timeline</div>
+                <div id="menu_button">About</div>
+                <div id="menu_button">Friends</div>
+                <div id="menu_button">Photos</div>
+                <div id="menu_button">Settings</div>
+            </div>
+        </div>
+        <div id="user_area" style="display: flex;">
+        <!--Friends Area-->
+            <div style="flex: 1;">
+
+                <div id="friends_bar">
+                    Friends <br>
+                    <div id="friends">
+                        <img id="friends_img"  src="img/Andrea.jpg" alt="portrait of Andrea">
+                        <br>
+                        Andrea Marks
+                    </div>
+                    <div id="friends">
+                        <img id="friends_img"  src="img/Brandy.jpg" alt="portrait of Brandy">
+                        <br>
+                        Brandy Greer
+                    </div>
+                    <div id="friends">
+                        <img id="friends_img"  src="img/Duncan.jpg" alt="portrait of Duncan">
+                        <br>
+                        Duncan Rogers
+                    </div>
+                    <div id="friends">
+                        <img id="friends_img"  src="img/Mathew.jpg" alt="portrait of Mathew">
+                        <br>
+                        Mathew Smith
+                    </div>
+                </div>
+            </div> 
+
+
+                <!--Timeline Area-->
+                <div style="flex: 2.5;"></div>
+            </div>
+        </div>
+
         <script src="" async defer></script>
     </body>
 </html>
